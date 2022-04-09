78548210570045c8d5249b58cf8f636a40135b4a
Magesh <mahesh00666@gmail.com>
2022-04-09 09:24:29 +0530

    m

---
+++ B.java
@@ -0,0 +1,15 @@
+package org.cts;
+
+public class B {
+	//Print odd number in b/w 1to 100
+	public static void main(String[] args) {
+		for(int i=0;i<=100;i++) {
+			if(i%2==1) {
+				System.out.println(i);
+				
+			}
+		}
+		
+	}
+
+}
